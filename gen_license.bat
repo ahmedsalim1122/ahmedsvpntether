5d443d1653f5e234cd14ccbb7fce2c62a251af83
ahmedsalim1122 <mohanedmajeed1@gmail.com>
2026-07-29 17:51:01 -0700

    Update gen_license.bat

--- gen_license.bat
+++ gen_license.bat
@@ -1,17 +1,1 @@
-@echo off
-REM gen_license.bat - Generate VPNTether license key from device UUID
-REM Usage: gen_license.bat <device-uuid>
-REM Example: gen_license.bat 28E24CE2-BA1C-38B1-AC56-C0BE08A077BC
 
-if "%1"=="" (
-    echo Usage: %0 ^<device-uuid^>
-    echo Example: %0 28E24CE2-BA1C-38B1-AC56-C0BE08A077BC
-    exit /b 1
-)
-
-setlocal enabledelayedexpansion
-set "uuid=%1"
-set "secret=VPNTetherSecret2026"
-set "keystr=%secret%%uuid%"
-
-powershell -Command "$h=(New-Object Security.Cryptography.SHA256Managed).ComputeHash([Text.Encoding]::UTF8.GetBytes('%keystr%'));$k='';$h[0..15]|%%{$k+=$_.ToString('x2')};Write-Output $k"
